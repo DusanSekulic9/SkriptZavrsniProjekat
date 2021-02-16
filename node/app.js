66a5437066dbe0413431249cff9c62e4688e5cde
DusanSekulic9 <dukiseki@gmail.com>
2021-02-16 17:48:25 +0100

    Login i register u node-u

--- node/app.js
+++ node/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const telefoni = require('./routes/TelefoniCrud');
+const telefoni = require('./routes/Baza');
 //const mysql = require('mysql');
 let port = process.env.PORT | 80;
 const history = require('connect-history-api-fallback');
--- node/routes/Baza.js
+++ node/routes/Baza.js
@@ -10,8 +10,12 @@ const pool = mysql.createPool({
     database: 'test'
 });
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 
 const route = express.Router();
+const bcrypt = require('bcrypt');
 
 
 const sema = Joi.object().keys({
@@ -24,6 +28,102 @@ const sema = Joi.object().keys({
 
 const link = Joi.object().keys({
     id: Joi.number().min(1).max(50).required()
+});
+
+const userSema = Joi.object().keys({
+    username : Joi.string().min(5).max(40).required(),
+    password : Joi.string().min(5).max(15).required(),
+    name : Joi.string().min(2).max(20),
+    email : Joi.string().email()
+})
+
+const loginSema = Joi.object().keys({
+    username : Joi.string().min(5).max(40).required(),
+    password : Joi.string().min(5).max(15).required()
+})
+async function hashPassword(password) {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    console.log(hash)
+}
+
+
+
+route.post('/register',jsonParser, (req,res)=>{
+    let { error } = userSema.validate(req.body);
+    if (error){
+        console.log(error)
+        res.status(400).send(error.details[0].message);
+    }else{
+        const name = req.body.username;
+        const email= req.body.email;
+        var password= req.body.password;
+        var name1 = req.body.name;
+        let errors = [];
+
+        //Check required fields
+        if(!name  || !password  ){
+            errors.push({msg: 'Please fill in all the fields'});
+            res.send({message:'Please fill in all the fields'});
+        }
+
+
+
+        if(errors.length>0){
+
+        }else{
+            if(email){
+                pool.query('SELECT * FROM users WHERE username = ?', [name],
+                    (error, results, fields)=>{
+                        if (results.length>0){
+                            res.send('username exists');
+                        }else{
+                            res.send('Reg success')
+                            bcrypt.hash(password, 5, (err, hash)=> {
+                                if(err)throw err;
+                                password = hash;
+                                pool.query('INSERT INTO users(username, email, password, name) VALUES(?,?,?,?)',
+                                    [name, email, password, name1]);
+                            });
+                            console.log("uspesna registracija")
+                        }
+
+                    });
+            }else{
+                res.send('Enter Email');
+            };
+        }
+    }
+
+});
+route.post('/login',jsonParser, (req, res)=> {
+    let { error } = loginSema.validate(req.body);
+    console.log(req.body.username)
+    console.log(req.body.password)
+    if (error){
+        res.status(400).send(error.details[0].message);
+    }else {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if (username && password) {
+            pool.query('SELECT * FROM users WHERE username = ?', [username],
+                (error, results, fields) => {
+                    pass = results[0].password;
+                    console.log(results[0]);
+                    console.log(pass);
+                    if (bcrypt.compareSync(password, pass)) {
+                        res.send(results[0]);
+                    } else {
+                        res.send('Incorrect Email and/or Password!');
+                    }
+                    res.end();
+                });
+        } else {
+            res.send('Please enter Username and Password!');
+            res.end();
+        }
+    }
 });
 
 
